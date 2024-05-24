3938907887beef1e9b51ad1f01fcee9ec2813df8
nbzhk <rumenovnikolay@gmail.com>
2024-05-24 14:03:16 +0200

    bootstrap for home, login and register

--- src/main/java/org/softuni/finalproject/validation/password/ConfirmPasswordValidator.java
+++ src/main/java/org/softuni/finalproject/validation/password/ConfirmPasswordValidator.java
@@ -4,9 +4,18 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import org.softuni.finalproject.model.dto.UserRegistrationDTO;
 
-public class ConfirmPasswordValidator implements ConstraintValidator<ConfirmPassword, UserRegistrationDTO> {
+public class ConfirmPasswordValidator implements ConstraintValidator<ConfirmPassword, Object> {
     @Override
-    public boolean isValid(UserRegistrationDTO userRegistrationDTO, ConstraintValidatorContext context) {
-        return userRegistrationDTO.getPassword().equals(userRegistrationDTO.getConfirmPassword());
+    public boolean isValid(Object object, ConstraintValidatorContext context) {
+        UserRegistrationDTO userRegistrationDTO = (UserRegistrationDTO) object;
+        boolean valid = userRegistrationDTO.getPassword().equals(userRegistrationDTO.getConfirmPassword());
+
+        if (!valid){
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(context.getDefaultConstraintMessageTemplate())
+                    .addPropertyNode("confirmPassword")
+                    .addConstraintViolation();
+        }
+        return valid;
     }
 }
