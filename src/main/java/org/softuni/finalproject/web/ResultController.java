2607e750ebaef08f4517550029bced65da8638e6
nbzhk <rumenovnikolay@gmail.com>
2024-05-25 14:26:34 +0200

    bootstrap

--- src/main/java/org/softuni/finalproject/web/ResultController.java
+++ src/main/java/org/softuni/finalproject/web/ResultController.java
@@ -1,14 +1,8 @@
 package org.softuni.finalproject.web;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-import org.softuni.finalproject.model.UserGuess;
 import org.softuni.finalproject.model.dto.GameDTO;
 import org.softuni.finalproject.service.GameService;
-import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
