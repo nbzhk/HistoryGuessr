89da86c9dcfd4659181a9f1c1014de4779d3f35d
nbzhk <rumenovnikolay@gmail.com>
2024-06-25 23:02:46 +0200

    Fixed User Validation

--- src/main/java/org/softuni/finalproject/config/SecurityConfiguration.java
+++ src/main/java/org/softuni/finalproject/config/SecurityConfiguration.java
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
@@ -54,7 +54,7 @@ public class SecurityConfiguration {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new Pbkdf2PasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 
 
--- src/main/java/org/softuni/finalproject/config/converter/PasswordEncoderConverter.java
+++ src/main/java/org/softuni/finalproject/config/converter/PasswordEncoderConverter.java
@@ -3,15 +3,15 @@ package org.softuni.finalproject.config.converter;
 
 import org.modelmapper.Converter;
 import org.modelmapper.spi.MappingContext;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
 public class PasswordEncoderConverter implements Converter<String, String> {
 
     private final PasswordEncoder passwordEncoder;
 
     public PasswordEncoderConverter() {
-        this.passwordEncoder = new Pbkdf2PasswordEncoder();
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
 
