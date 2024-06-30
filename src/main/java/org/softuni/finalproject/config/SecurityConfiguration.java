85a7efb0f8da9e4ea4c8977543d724e08a25db7e
nbzhk <rumenovnikolay@gmail.com>
2024-07-01 00:26:33 +0200

    bootstrap

--- src/main/java/org/softuni/finalproject/config/SecurityConfiguration.java
+++ src/main/java/org/softuni/finalproject/config/SecurityConfiguration.java
@@ -22,6 +22,7 @@ public class SecurityConfiguration {
         http
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/webjars/**").permitAll()
                         .requestMatchers("/", "/users/login", "/users/register", "/users/login-error").permitAll()
                         .requestMatchers("/users/delete/**","/users/make-admin/**","/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
