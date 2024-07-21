ba24d59d2b02f755609dbea915d76467910232c0
nbzhk <rumenovnikolay@gmail.com>
2024-07-21 17:57:04 +0200

    home page for authenticated and unauthenticated user

--- src/main/java/org/softuni/finalproject/web/HomeController.java
+++ src/main/java/org/softuni/finalproject/web/HomeController.java
@@ -1,13 +1,23 @@
 package org.softuni.finalproject.web;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-   @GetMapping("/")
-    public String home() {
+
+    @GetMapping("/")
+    public String homeNotLoggedIn(@CurrentSecurityContext SecurityContext context) {
+
+        if (context.getAuthentication() instanceof AnonymousAuthenticationToken) {
+            return "index";
+        }
+
         return "home";
     }
+
 }
